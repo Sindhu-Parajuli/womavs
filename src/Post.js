b4fba6ebb567d4f37d481be488d60764b56cecce
Tolulope0 <44985202+Tolulope0@users.noreply.github.com>
2020-10-19 23:45:57 -0500

    Implemented Post Feature
    Reimplemented functionality of Post Feature using Firestore.

---
+++ src/Post.js
@@ -0,0 +1,44 @@
+import capture from "./images/Capture.PNG";
+import propic from "./images/default.PNG"
+import React, {useEffect, useState} from "react";
+import App from "./App";
+import hp from "./css/hp.css"
+import {useHistory} from "react-router-dom";
+import firebase from "./firebase.js";
+import {Navbar,Nav} from "react-bootstrap";
+import {Avatar} from "@material-ui/core";
+
+
+const Post = ({username, timestamp, userImage,post}) =>{
+
+    return(
+        <div className="post_body">
+            <div className="card px-3 py-4 " style={{marginTop: 20}}>
+                    <div className={"row"} style={{marginLeft:5}}>
+                        <Avatar src={userImage}></Avatar>
+                        <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
+                    </div>
+                    <div id={"post_header"}>
+                    <h1>{username}</h1>
+
+                        <p>{post}</p>
+                    </div>
+                        <ul className="list-group">
+                        <button className="btn float-Center" style={{
+                            display: 'flex',
+                            background: "rgb(0,100,177)",
+                            justifyContent: 'center',
+                            alignSelf: 'center',
+                            height: 40,
+                            width: 200
+                        }}
+                        >Comment
+                        </button>
+
+                    </ul>
+                </div>
+
+        </div>
+    );
+}
+export default Post;
